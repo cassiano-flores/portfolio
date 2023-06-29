82dce8df6d1899259a6deab94101f0adedac4324
Cassiano Flores <cassiano.flores2424@gmail.com>
2023-06-29 14:49:34 -0300

    background updates

--- src/ui/screens/home/home.screen.jsx
+++ src/ui/screens/home/home.screen.jsx
@@ -34,34 +34,45 @@ export function HomeScreen() {
     }
 
     return (
-        <div className="container">
-            {/* Welcome */}
-            <section className="first-section">
-                <div className="overlay"></div>
-                <h1>Cassiano Flores</h1>
+        <>
+            <div className="container">
+                {/* Welcome */}
+                <section className="first-section">
+                    <div className="overlay"></div>
+                    <h1>Cassiano Flores</h1>
 
-                <Typed className="fs-h2" strings={TITLES} typeSpeed={100} backSpeed={80} backDelay={2000} showCursor shuffle loop />
+                    <Typed
+                        className="fs-h2"
+                        strings={TITLES}
+                        typeSpeed={100}
+                        backSpeed={80}
+                        backDelay={2000}
+                        showCursor
+                        shuffle
+                        loop
+                    />
 
-                <div className="fs--contact-container">
-                    <ContactCircle url={URLS.github} icon={faGithub} />
-                    <ContactCircle url={URLS.linkedin} icon={faLinkedin} />
-                    <ContactCircle url={URLS.instagram} icon={faInstagram} />
-                    <ContactCircle icon={faEnvelope} handleClick={scrollContact} openInNewTab={false} />
-                </div>
+                    <div className="fs--contact-container">
+                        <ContactCircle url={URLS.github} icon={faGithub} />
+                        <ContactCircle url={URLS.linkedin} icon={faLinkedin} />
+                        <ContactCircle url={URLS.instagram} icon={faInstagram} />
+                        <ContactCircle icon={faEnvelope} handleClick={scrollContact} openInNewTab={false} />
+                    </div>
 
-                <FontAwesomeIcon className="scroll-button" icon={faAngleDoubleDown} onClick={scrollSection} />
-            </section>
+                    <FontAwesomeIcon className="scroll-button" icon={faAngleDoubleDown} onClick={scrollSection} />
+                </section>
 
-            {/* Projects */}
-            <section className="second-section">
-                <h1>Projects</h1>
+                {/* Projects */}
+                <section className="second-section">
+                    <h1>Projects</h1>
 
-                <div className="ss--project-container">
-                    {PROJECTS.map((project, index) => (
-                        <ProjectCard key={index} image={project.image} description={project.description} url={project.url} />
-                    ))}
-                </div>
-            </section>
+                    <div className="ss--project-container">
+                        {PROJECTS.map((project, index) => (
+                            <ProjectCard key={index} image={project.image} description={project.description} url={project.url} />
+                        ))}
+                    </div>
+                </section>
+            </div>
 
             {/* Contact */}
             <section className="third-section">
@@ -100,6 +111,101 @@ export function HomeScreen() {
                     </div>
                 </div>
             </section>
-        </div>
+
+            <div id="bg-wrap">
+                <svg viewBox="0 0 115 120" preserveAspectRatio="xMidYMid slice">
+                    <defs>
+                        <radialGradient id="Gradient1" cx="50%" cy="50%" fx="0.441602%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="34s" values="0%;3%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(255, 0, 255, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(255, 0, 255, 0)"></stop>
+                        </radialGradient>
+                        <radialGradient id="Gradient2" cx="50%" cy="50%" fx="2.68147%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="23.5s" values="0%;3%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(255, 255, 0, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(255, 255, 0, 0)"></stop>
+                        </radialGradient>
+                        <radialGradient id="Gradient3" cx="50%" cy="50%" fx="0.836536%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="21.5s" values="0%;3%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(0, 255, 255, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(0, 255, 255, 0)"></stop>
+                        </radialGradient>
+                        <radialGradient id="Gradient4" cx="50%" cy="50%" fx="4.56417%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="23s" values="0%;5%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(0, 255, 0, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(0, 255, 0, 0)"></stop>
+                        </radialGradient>
+                        <radialGradient id="Gradient5" cx="50%" cy="50%" fx="2.65405%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="24.5s" values="0%;5%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(0,0,255, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(0,0,255, 0)"></stop>
+                        </radialGradient>
+                        <radialGradient id="Gradient6" cx="50%" cy="50%" fx="0.981338%" fy="50%" r=".5">
+                            <animate attributeName="fx" dur="25.5s" values="0%;5%;0%" repeatCount="indefinite"></animate>
+                            <stop offset="0%" stop-color="rgba(255,0,0, 1)"></stop>
+                            <stop offset="100%" stop-color="rgba(255,0,0, 0)"></stop>
+                        </radialGradient>
+                    </defs>
+
+                    <rect
+                        x="13.744%"
+                        y="1.18473%"
+                        width="100%"
+                        height="100%"
+                        fill="url(#Gradient1)"
+                        transform="rotate(334.41 50 50)"
+                    >
+                        <animate attributeName="x" dur="20s" values="25%;0%;25%" repeatCount="indefinite"></animate>
+                        <animate attributeName="y" dur="21s" values="0%;25%;0%" repeatCount="indefinite"></animate>
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="0 50 50"
+                            to="360 50 50"
+                            dur="7s"
+                            repeatCount="indefinite"
+                        ></animateTransform>
+                    </rect>
+                    <rect
+                        x="-2.17916%"
+                        y="35.4267%"
+                        width="100%"
+                        height="100%"
+                        fill="url(#Gradient2)"
+                        transform="rotate(255.072 50 50)"
+                    >
+                        <animate attributeName="x" dur="23s" values="-25%;0%;-25%" repeatCount="indefinite"></animate>
+                        <animate attributeName="y" dur="24s" values="0%;50%;0%" repeatCount="indefinite"></animate>
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="0 50 50"
+                            to="360 50 50"
+                            dur="12s"
+                            repeatCount="indefinite"
+                        ></animateTransform>
+                    </rect>
+                    <rect
+                        x="9.00483%"
+                        y="14.5733%"
+                        width="100%"
+                        height="100%"
+                        fill="url(#Gradient3)"
+                        transform="rotate(139.903 50 50)"
+                    >
+                        <animate attributeName="x" dur="25s" values="0%;25%;0%" repeatCount="indefinite"></animate>
+                        <animate attributeName="y" dur="12s" values="0%;25%;0%" repeatCount="indefinite"></animate>
+                        <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            from="360 50 50"
+                            to="0 50 50"
+                            dur="9s"
+                            repeatCount="indefinite"
+                        ></animateTransform>
+                    </rect>
+                </svg>
+            </div>
+        </>
     );
 }
